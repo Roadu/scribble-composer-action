1db61a3f6198a4bd6439adce20ed8022beea24b3
Roadu <roadu_202@proton.me>
2025-03-03 10:16:23 -0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -88,4 +88,15 @@ app/*
     );
   });
 
+
+  const isCloudflareWorker = typeof caches !== 'undefined'
+if (isCloudflareWorker) {
+  const manifest = await import('__STATIC_CONTENT_MANIFEST')
+  const serveStaticOptions = { manifest, root: './' }
+  app.use('/*', serveStatic(serveStaticOptions))
+  devtools(app, { assetsPath: '/frog', serveStatic, serveStaticOptions })
+} else {
+  devtools(app, { serveStatic })
+}
+
 export default app;
